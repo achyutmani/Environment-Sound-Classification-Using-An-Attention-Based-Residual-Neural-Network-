524f27dfad7173be8f855f05d7affe9865780875
Achyut Mani Tripathi <achyutmani9@gmail.com>
2021-10-15 13:37:23 +0530

    Update Attention.py

--- Attention.py
+++ Attention.py
@@ -13,7 +13,7 @@ from torch.nn import functional as F
 from torch.autograd import Variable
 torch_ver = torch.__version__[:3]
 
-__all__ = ['PAM_Module', 'CAM_Module','SA_Module']
+__all__ = ['PAM_Module']
 
 
 class PAM_Module(Module):
@@ -51,67 +51,3 @@ class PAM_Module(Module):
         return out
 
 
-class CAM_Module(Module):
-    """ Channel attention module"""
-    def __init__(self, in_dim):
-        super(CAM_Module, self).__init__()
-        self.chanel_in = in_dim
-
-
-        self.gamma = Parameter(torch.zeros(1))
-        self.softmax  = Softmax(dim=-1)
-    def forward(self,x):
-        """
-            inputs :
-                x : input feature maps( B X C X H X W)
-            returns :
-                out : attention value + input feature
-                attention: B X C X C
-        """
-        m_batchsize, C, height, width = x.size()
-        proj_query = x.view(m_batchsize, C, -1)
-        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)
-        energy = torch.bmm(proj_query, proj_key)
-        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
-        attention = self.softmax(energy_new)
-        proj_value = x.view(m_batchsize, C, -1)
-
-        out = torch.bmm(attention, proj_value)
-        out = out.view(m_batchsize, C, height, width)
-
-        out = self.gamma*out + x
-        return out
-
-class SA_Module(nn.Module):
-    """ Self attention Layer"""
-    def __init__(self,in_dim,activation):
-        super(SA_Module,self).__init__()
-        self.chanel_in = in_dim
-        self.activation = activation
-        
-        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//2, kernel_size= 1)
-        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//2 , kernel_size= 1)
-        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
-        self.gamma = nn.Parameter(torch.zeros(1))
-
-        self.softmax  = nn.Softmax(dim=-1) #
-    def forward(self,x):
-        """
-            inputs :
-                x : input feature maps( B X C X W X H)
-            returns :
-                out : self attention value + input feature 
-                attention: B X N X N (N is Width*Height)
-        """
-        m_batchsize,C,width ,height = x.size()
-        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B X CX(N)
-        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
-        energy =  torch.bmm(proj_query,proj_key) # transpose check
-        attention = self.softmax(energy) # BX (N) X (N) 
-        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
-
-        out = torch.bmm(proj_value,attention.permute(0,2,1) )
-        out = out.view(m_batchsize,C,width,height)
-        
-        out = self.gamma*out + x
-        return out       
